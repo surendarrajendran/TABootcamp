51b4b73a9165ecacaaa2235052d43106d20420e6
Surendar Rajendran <surendarrajendran16@gmail.com>
2026-04-07 15:15:15 +0300

    Added FindLargest class

---
+++ src/main/java/com/bootcampexcercise/module2/activity/FindLargest.java
@@ -0,0 +1,23 @@
+package com.bootcampexcercise.module2.activity;
+
+public class FindLargest {
+
+    public static void main(String args[]) {
+
+        int x = 10;
+        int y = 88;
+        int z = 2;
+        int large;
+
+        if (x >= y && x >=z) {          // check if x is larger than y and z
+            large = x;
+        } else if (y >= x && y>=z) {    // check if y is larger than x and z
+            large = y;
+        } else {
+            large = z;                  // assign z as largest if x and y are not the largest
+        }
+
+        // Print result
+        System.out.println("Largest Number is: " + large);
+    }
+}
