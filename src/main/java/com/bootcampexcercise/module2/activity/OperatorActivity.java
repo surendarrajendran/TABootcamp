67d570aeb033d1538d7feeb68c07e1ad0cc4191f
Surendar Rajendran <surendarrajendran16@gmail.com>
2026-04-07 14:59:38 +0300

    Added OperatorActivity class

---
+++ src/main/java/com/bootcampexcercise/module2/activity/OperatorActivity.java
@@ -0,0 +1,14 @@
+package com.bootcampexcercise.module2.activity;
+
+public class OperatorActivity {
+    public static void main(String[] args) {
+
+        // Input integers
+        int x = 10;
+        int y = 20;
+
+        int sum = x + y; // sum up input
+
+        System.out.println("Sum: " + sum); // print the result
+    }
+}
