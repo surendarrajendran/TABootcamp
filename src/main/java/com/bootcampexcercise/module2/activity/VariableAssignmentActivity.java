026712d08273b37af881c9dbfcf34a2c1b1335df
Surendar Rajendran <surendarrajendran16@gmail.com>
2026-04-07 14:27:32 +0300

    Added Java project files

---
+++ src/main/java/com/bootcampexcercise/module2/activity/VariableAssignmentActivity.java
@@ -0,0 +1,12 @@
+package com.bootcampexcercise.module2.activity;
+
+public class VariableAssignmentActivity {
+    public static void main(String[] args) {
+
+        int number = 0;
+
+        number++;
+
+        System.out.println("Updated value: " + number);
+    }
+}
